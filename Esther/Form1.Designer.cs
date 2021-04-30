d4849c0894ec9a1c36b7e097fc5038ffabe02b79
Touraj Ostovari <Toraj.ostovari@gmail.com>
2021-04-30 19:51:22 +0430

    Add project files.

---
+++ Esther/Form1.Designer.cs
@@ -0,0 +1,300 @@
+﻿
+namespace Esther
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.preview_txt = new System.Windows.Forms.RichTextBox();
+            this.json_txt = new System.Windows.Forms.RichTextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.Delete = new System.Windows.Forms.RadioButton();
+            this.Put = new System.Windows.Forms.RadioButton();
+            this.Post = new System.Windows.Forms.RadioButton();
+            this.Get = new System.Windows.Forms.RadioButton();
+            this.url_txt = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Emoji", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(61)))), ((int)(((byte)(142)))), ((int)(((byte)(185)))));
+            this.label1.Location = new System.Drawing.Point(13, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(277, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Esther Web API Tester APP | Free Version";
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label2.Location = new System.Drawing.Point(13, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(943, 10);
+            this.label2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Segoe UI Historic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(881, 1);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "X";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.preview_txt);
+            this.groupBox1.Controls.Add(this.json_txt);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.groupBox2);
+            this.groupBox1.Controls.Add(this.url_txt);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Font = new System.Drawing.Font("Segoe UI Symbol", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.groupBox1.Location = new System.Drawing.Point(13, 42);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(949, 476);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Lab:";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(221)))), ((int)(((byte)(187)))));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(228)))), ((int)(((byte)(204)))));
+            this.button2.Location = new System.Drawing.Point(771, 128);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(172, 342);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "... Execute ...";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // preview_txt
+            // 
+            this.preview_txt.Enabled = false;
+            this.preview_txt.Location = new System.Drawing.Point(6, 234);
+            this.preview_txt.Name = "preview_txt";
+            this.preview_txt.Size = new System.Drawing.Size(757, 236);
+            this.preview_txt.TabIndex = 6;
+            this.preview_txt.Text = "";
+            // 
+            // json_txt
+            // 
+            this.json_txt.Location = new System.Drawing.Point(7, 106);
+            this.json_txt.Name = "json_txt";
+            this.json_txt.Size = new System.Drawing.Size(757, 96);
+            this.json_txt.TabIndex = 5;
+            this.json_txt.Text = "";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(7, 214);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(55, 17);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Preview:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(7, 86);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(76, 17);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Json Query:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.Delete);
+            this.groupBox2.Controls.Add(this.Put);
+            this.groupBox2.Controls.Add(this.Post);
+            this.groupBox2.Controls.Add(this.Get);
+            this.groupBox2.Location = new System.Drawing.Point(770, 45);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(173, 76);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            // 
+            // Delete
+            // 
+            this.Delete.AutoSize = true;
+            this.Delete.Location = new System.Drawing.Point(87, 41);
+            this.Delete.Name = "Delete";
+            this.Delete.Size = new System.Drawing.Size(69, 21);
+            this.Delete.TabIndex = 1;
+            this.Delete.Text = "DELETE";
+            this.Delete.UseVisualStyleBackColor = true;
+            // 
+            // Put
+            // 
+            this.Put.AutoSize = true;
+            this.Put.Location = new System.Drawing.Point(87, 16);
+            this.Put.Name = "Put";
+            this.Put.Size = new System.Drawing.Size(49, 21);
+            this.Put.TabIndex = 1;
+            this.Put.Text = "PUT";
+            this.Put.UseVisualStyleBackColor = true;
+            // 
+            // Post
+            // 
+            this.Post.AutoSize = true;
+            this.Post.Location = new System.Drawing.Point(8, 41);
+            this.Post.Name = "Post";
+            this.Post.Size = new System.Drawing.Size(57, 21);
+            this.Post.TabIndex = 0;
+            this.Post.Text = "POST";
+            this.Post.UseVisualStyleBackColor = true;
+            // 
+            // Get
+            // 
+            this.Get.AutoSize = true;
+            this.Get.Checked = true;
+            this.Get.Location = new System.Drawing.Point(8, 14);
+            this.Get.Name = "Get";
+            this.Get.Size = new System.Drawing.Size(49, 21);
+            this.Get.TabIndex = 0;
+            this.Get.TabStop = true;
+            this.Get.Text = "GET";
+            this.Get.UseVisualStyleBackColor = true;
+            // 
+            // url_txt
+            // 
+            this.url_txt.Location = new System.Drawing.Point(7, 46);
+            this.url_txt.Name = "url_txt";
+            this.url_txt.Size = new System.Drawing.Size(757, 25);
+            this.url_txt.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(778, 25);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 17);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Method:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 25);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 17);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "URL:";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 532);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(42, 15);
+            this.label7.TabIndex = 4;
+            this.label7.Text = "Status:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(61, 532);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(12, 15);
+            this.label8.TabIndex = 5;
+            this.label8.Text = "-";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(198)))), ((int)(((byte)(6)))));
+            this.ClientSize = new System.Drawing.Size(974, 565);
+            this.ControlBox = false;
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Esther | WebAPI Tester";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton Put;
+        private System.Windows.Forms.RadioButton Post;
+        private System.Windows.Forms.RadioButton Get;
+        private System.Windows.Forms.TextBox url_txt;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RadioButton Delete;
+        private System.Windows.Forms.RichTextBox preview_txt;
+        private System.Windows.Forms.RichTextBox json_txt;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ Esther/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Esther
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.ExitThread();
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            if (Uri.IsWellFormedUriString(url_txt.Text, UriKind.Absolute))
+            {
+                if (Get.Checked)
+                {
+                    if (json_txt.Text.Length == 0)
+                    {
+                        System.Net.Http.HttpRequestMessage requestMessage = new System.Net.Http.HttpRequestMessage(System.Net.Http.HttpMethod.Get, url_txt.Text);
+                        System.Net.Http.HttpClient http = new System.Net.Http.HttpClient();
+                        System.Net.Http.HttpResponseMessage httpResponse = await http.SendAsync(requestMessage);
+                        preview_txt.Text = httpResponse.Content.ReadAsStringAsync().Result;
+                        label8.Text = httpResponse.StatusCode.ToString();
+                    }
+                    else
+                    {
+                        MessageBox.Show("You can not send json-query to GET method","Info",MessageBoxButtons.OK,MessageBoxIcon.Information);
+                    }
+                }
+                if (Post.Checked)
+                {
+                    var data = new System.Net.Http.StringContent(json_txt.Text, Encoding.UTF8, "application/json");
+                    System.Net.Http.HttpClient http = new System.Net.Http.HttpClient();
+                    System.Net.Http.HttpResponseMessage httpResponse = await http.PostAsync(url_txt.Text, data);
+                    preview_txt.Text = httpResponse.Content.ReadAsStringAsync().Result;
+                }
+                if (Put.Checked)
+                {
+                    var data = new System.Net.Http.StringContent(json_txt.Text, Encoding.UTF8, "application/json");
+                    System.Net.Http.HttpClient http = new System.Net.Http.HttpClient();
+                    System.Net.Http.HttpResponseMessage httpResponse = await http.PutAsync(url_txt.Text, data);
+                    preview_txt.Text = httpResponse.Content.ReadAsStringAsync().Result;
+                }
+                if (Delete.Checked)
+                {
+                    System.Net.Http.HttpRequestMessage requestMessage = new System.Net.Http.HttpRequestMessage(System.Net.Http.HttpMethod.Delete, url_txt.Text);
+                    System.Net.Http.HttpClient http = new System.Net.Http.HttpClient();
+                    System.Net.Http.HttpResponseMessage httpResponse = await http.SendAsync(requestMessage);
+                    preview_txt.Text = httpResponse.Content.ReadAsStringAsync().Result;
+                }
+            }
+        }
+    }
+}
